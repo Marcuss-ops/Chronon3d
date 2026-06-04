b993f1c5d7462ce24614c11f3b5f9097a8584f4f
Codebuff User <user@codebuff.local>
2026-06-04 14:07:55 +0000

    chore: remove dead text_registrations.cpp and empty register_all() from 2d5 compositions

--- content/2d5/compositions/two_point_five_d_compositions.cpp
+++ content/2d5/compositions/two_point_five_d_compositions.cpp
@@ -1,4 +1,3 @@
-#include <chronon3d/core/composition/composition_registration.hpp>
 #include <chronon3d/core/types/frame_context.hpp>
 #include <chronon3d/timeline/composition.hpp>
 #include <chronon3d/scene/builders/scene_builder.hpp>
@@ -368,8 +367,4 @@ Composition dof_showcase() {
     });
 }
 
-void register_all() {}
-
 } // namespace chronon3d::content::two_point_five_d
-
-// Registrations are now handled by TwoPointFiveDModule in two_point_five_d_module.cpp
