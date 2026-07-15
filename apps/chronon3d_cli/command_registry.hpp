46ac06b0ade57898b3797e91dbf907529805894e
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 09:23:47 +0000

    refactor(cli): remove deprecated video registry surface

--- apps/chronon3d_cli/command_registry.hpp
+++ apps/chronon3d_cli/command_registry.hpp
@@ -17,7 +17,6 @@ namespace chronon3d::cli {
 
 void register_render_commands(CLI::App& app, CliContext& ctx);
 void register_validate_commands(CLI::App& app, CliContext& ctx);
-void register_video_commands(CLI::App& app, CliContext& ctx);
 void register_dev_commands(CLI::App& app, CliContext& ctx);
 void register_inspect_commands(CLI::App& app, CliContext& ctx);
 // TICKET-V3-CLI-UNIFICATION-STARTER-TEMPLATE (Blocco 4.2)
@@ -26,7 +25,6 @@ void register_create_commands(CLI::App& app, CliContext& ctx);
 void register_watch_commands(CLI::App& app, CliContext& ctx);
 // TICKET-V3-CLI-UNIFICATION-PREVIEW (Blocco 4.1)
 void register_preview_commands(CLI::App& app, CliContext& ctx);
-
 
 #ifdef CHRONON3D_BUILD_BENCHMARKS
 void register_bench_commands(CLI::App& app, CliContext& ctx);
