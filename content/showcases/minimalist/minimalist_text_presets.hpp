c080f2a974a2ec3a1bc256911c653c8eb4c51117
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 10:43:26 +0000

    refactor(minimalist): declare text preset factories

---
+++ content/showcases/minimalist/minimalist_text_presets.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chronon3d/timeline/composition.hpp>
+
+namespace chronon3d::content::minimalist {
+
+Composition minimalist_text_fade_up();
+Composition minimalist_text_tracking_reveal();
+Composition minimalist_text_clip_reveal();
+Composition minimalist_text_fade_down();
+Composition minimalist_text_soft_scale();
+Composition minimalist_text_blur_focus();
+Composition minimalist_text_slide_left();
+Composition minimalist_text_slide_right();
+Composition minimalist_text_scale_pop();
+Composition minimalist_text_float_in();
+Composition minimalist_text_letter_rise();
+Composition minimalist_text_drift_in();
+Composition minimalist_text_tilt_in();
+Composition minimalist_text_mask_reveal();
+Composition minimalist_text_snap_pop();
+
+Composition minimalist_text_fade_away();
+Composition minimalist_text_scale_out();
+Composition minimalist_text_slide_up_out();
+Composition minimalist_text_blur_away();
+Composition minimalist_text_tilt_out();
+
+Composition minimalist_text_float_in_no_glow();
+
+} // namespace chronon3d::content::minimalist
