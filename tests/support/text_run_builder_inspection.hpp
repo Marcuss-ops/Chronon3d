82e2896e52ab343eaed597a987c69e669ed5e263
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 10:44:07 +0000

    test(authoring): delete redundant inspector header

--- tests/support/text_run_builder_inspection.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Temporary forwarding include while the remaining authoring test files migrate
-// to their canonical shared support header. Delete this file after the includes
-// of ../support/text_run_builder_inspection.hpp are removed.
-#include "../authoring/authoring_dsl_test_support.hpp"
