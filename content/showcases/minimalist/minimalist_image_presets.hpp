d954444e624ed383cdae6fd257094396a77d68d5
PierThatDev <futurimilionariposta@gmail.com>
2026-07-15 10:46:46 +0000

    refactor(minimalist): declare image preset factories

---
+++ content/showcases/minimalist/minimalist_image_presets.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <chronon3d/timeline/composition.hpp>
+
+namespace chronon3d::content::minimalist {
+
+Composition minimalist_image_fade_in();
+Composition minimalist_image_focus_in();
+Composition minimalist_image_scale_drop();
+Composition minimalist_image_fade_shift_vertical();
+Composition minimalist_image_center_split();
+Composition minimalist_image_reveal_from_bottom();
+Composition minimalist_image_framing_bracket();
+Composition minimalist_image_tracking_breathing();
+Composition minimalist_image_elegant_exit();
+Composition minimalist_image_elastic_slide();
+
+} // namespace chronon3d::content::minimalist
