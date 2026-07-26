def1cc4ff60157d2012ce45c9289540f75a94e10
Marcuss-ops <marcuss-ops@users.noreply.github.com>
2026-07-26 21:07:32 +0000

    chore(gates): forbid process-wide cache state

--- tools/gates/manifest.sh
+++ tools/gates/manifest.sh
@@ -30,6 +30,7 @@ DEVELOPER_GATES=(
     check_commit_subject_length.sh
     check_push_divergence_window.sh
     check_unique_cmake_source_ownership.py
+    check_no_process_wide_caches.sh
     check_architecture_boundaries.sh
 )
 
