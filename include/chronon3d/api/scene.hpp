4447e6e598c2dca860c40747ba1df36b8160f190
PierThatDev <futurimilionariposta@gmail.com>
2026-09-02 16:50:34 +0000

    cleanup: remove obsolete scene forwarding header

--- include/chronon3d/api/scene.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Public scene entry point. The implementation lives in the scene model;
-// this stable include keeps authoring code independent of that layout.
-#include <chronon3d/scene/model/core/scene.hpp>
